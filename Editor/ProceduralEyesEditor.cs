92c6288caa143928b84b0691327dd62aeadd0b40
YooPita <34219009+YooPita@users.noreply.github.com>
2018-08-21 21:32:13 +0300

    update

--- Editor/ProceduralEyesEditor.cs
+++ Editor/ProceduralEyesEditor.cs
@@ -1,48 +1,8 @@
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
-
-public class ProceduralEyes : MonoBehaviour
-{
-    public float maxZFar = -5;
-    public List<Eye> Eyes = new List<Eye>();
-
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        foreach(Eye a in Eyes)
-        {
-            if(a.eye!=null)Gizmos.DrawWireSphere(a.eye.position, a.radius);
-        }
-    }
-    
-    void Update ()
-    {
-        foreach (Eye a in Eyes)
-        {
-            Vector2 controller;
-            float lerpVal = (maxZFar - transform.position.z) / maxZFar;
-            controller = Vector2.Lerp(a.eye.localPosition, transform.localPosition, lerpVal / 2);
-
-            var leftPos = Vector2.ClampMagnitude(controller - (Vector2)a.eye.localPosition, a.radius);//Normalization
-            if (Quaternion.Angle(a.eye.rotation, transform.rotation) > 60)
-            {
-                leftPos.x *= -1;
-            }
-
-            a.eye.GetChild(0).localPosition = leftPos;//The first child is the pupil 
-        }
-    }
-
-}
-
-[System.Serializable]
-public struct Eye
-{
-    public Transform eye;
-    public float radius;
-}
 
 [CustomEditor(typeof(ProceduralEyes))]
 public class ProceduralEyesEditor : Editor
@@ -71,6 +31,7 @@ public class ProceduralEyesEditor : Editor
     public override void OnInspectorGUI()
     {
         ProceduralEyes myTarget = (ProceduralEyes)target;
+        myTarget.preview = EditorGUILayout.Toggle("Preview", myTarget.preview);
         myTarget.maxZFar = EditorGUILayout.FloatField("Max Z Far", myTarget.maxZFar);
 
         serializedObject.Update();
